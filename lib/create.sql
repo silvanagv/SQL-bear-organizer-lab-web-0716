6f67f82881cc3c53219eb5609951d5efa5cb15f1
silvanagv <silvanagoberdhanvigle@gmail.com>
2016-08-03 21:36:39 -0400

    solved

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears
+(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INT,
+  gender CHAR(1),
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO bears (name)
+VALUES ('Mr. Chocolate'), ('Rowdy'), ('Tabitha'), ('Sergeant Brown'), ('Melissa'), ('Grinch'), ('Wendy'), (null);
